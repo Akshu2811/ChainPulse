a937c9bbd562eeeac2b55e33c33f6dc1ab89b597
Akshitha Ganji <akshithaganji19@gmail.com>
2026-04-26 00:42:01 +0530

    feat: initial ChainPulse Spring Boot scaffold

---
+++ src/main/java/com/chainpulse/chainpluse/ChainPulseApplication.java
@@ -0,0 +1,13 @@
+package com.chainpulse.chainpluse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainPulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChainPulseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chainpulse/chainpluse/ChainPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chainpulse.chainpluse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainPulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
